35da0e5b5f58f511ca99dba37bfbf16a239916f4
Tue <manhtuetruong@gmail.com>
2020-08-03 22:42:10 +1000

    add homepage and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header/Header';
+import Homepage from './views/Homepage/Homepage';
 
 function App() {
   return (
-    <div className="d-flex justify-content-around">
-      Hello World
-    </div>
+    <>
+    <Header />
+      <div className="app">
+        <Homepage />
+      </div>
+    </>
   );
 }
 
---
+++ src/views/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Homepage.scss';
+
+const Homepage = props => {
+  return (
+    <div className="homepage">
+      <div className="category buy-dog">
+        <button type="button" className="btn btn-lg btn-dark">Woof</button>
+      </div>
+      <div className="category buy-cat">
+        <button type="button" className="btn btn-lg btn-dark">Meow</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
